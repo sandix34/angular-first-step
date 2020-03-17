9336ef499c6fb7316aced79b2123bfe13046b57c
sandrine <sandrinezocca@gmail.com>
2020-03-17 12:55:02 +0100

    Template syntax

--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { products } from '../products';
 
 @Component({
   selector: 'app-product-list',
@@ -7,9 +8,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductListComponent implements OnInit {
 
+  public products: object = products;
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  share() {
+    window.alert('The product has been shared!');
   }
 
 }
